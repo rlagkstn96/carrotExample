bba334bef4f2256f18a9ac547e119c8fbd6bcc5d
upsight <upsight.kr@gmail.com>
2023-02-03 14:11:46 +0900

    0.0.1V carrot Application Start

---
+++ android/app/src/main/kotlin/com/example/carrotexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carrotexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 24,
+          width: 24,
+          child: CircularProgressIndicator(
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
